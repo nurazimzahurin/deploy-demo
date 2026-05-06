b0c5a608ef594f1866a98cf873579426d58fb0ca
azim.zahurin <azim.zahurin@razer.com>
2026-05-06 15:41:54 +0800

    fix storage permission issue

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+chown -R www-data:www-data /app/storage /app/bootstrap/cache
+chmod -R 775 /app/storage /app/bootstrap/cache
+
 nohup php-fpm > /dev/stdout 2>/dev/stderr &
 nginx -g "daemon off;" > /dev/stdout 2>/dev/stderr
